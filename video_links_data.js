c1032e87a11ec0d4c7cb604f1f28c44780425118
Isabelle M <isabelle.mah@gmail.com>
2016-04-04 20:55:26 +0200

    Updated links to only most often watched replay videos

--- video_links_data.js
+++ video_links_data.js
@@ -1,54 +1,18 @@
 videoLinksData = [
       {
-      name: "canalplus.fr - Zapping",
-      url: "http://www.canalplus.fr/c-infos-documentaires/pid1830-c-zapping.html"
-      },
-      {
-      name: "canalplus.fr - Made in Groland",
-      url: "http://www.canalplus.fr/c-divertissement/pid1787-c-groland.html"
-      },
-      {
-      name: "canalplus.fr - Les Guignols de l'Info",
-      url: "http://www.canalplus.fr/c-divertissement/pid1784-c-les-guignols.html"
-      },
-      {
-      name: "canalplus.fr - Pepites sur le net",
-      url: "http://www.canalplus.fr/c-divertissement/pid1778-c-pepites-sur-le-net.html"
-      },
-      {
-      name: "canalplus.fr - L'Oeil de Links",
-      url: "http://www.canalplus.fr/c-infos-documentaires/c-ms-l-oeil-de-links/pid4746-les-emissions.html"
-      },
-      {
-      name: "canalplus.fr - L'Album de la Semaine",
-      url: "http://www.canalplus.fr/c-divertissement/c-musique/pid5043-album-de-la-semaine.html"
-      },
-      {
       name: "numero23.fr - Rocking Chair",
       url: "http://www.numero23.fr/programmes/rocking-chair-3/"
       },
       {
-      name: "d17.tv - Le Zap",
-      url: "http://www.d17.tv/docs-mags/pid6275-le-zap.html"
+      name: "canalplus.fr - Zapping",
+      url: "http://www.canalplus.fr/c-infos-documentaires/pid1830-c-zapping.html"
       },
       {
-      name: "spi0n.com - Zapping web",
-      url: "http://www.spi0n.com/zapping-web/"
+      name: "canalplus.fr - Stephane Guillon",
+      url: "http://www.canalplus.fr/c-emissions/c-salut-les-terriens/pid8294-stephane-guillon.html"
       },
       {
-      name: "gameone.net - Funky web",
-      url: "http://www.gameone.net/programmes/funky-web/"
-      },
-      {
-      name: "youtube.com - TED Talks",
-      url: "http://www.youtube.com/user/TEDtalksDirector/videos?view=1&flow=list"
-      },
-      {
-      name: "youtube.com - TEDxTalks",
-      url: "http://www.youtube.com/user/TEDxTalks/videos?view=1&sort=dd&live_view=500&flow=list"
-      },
-      {
-      name: "youtube.com - TED-Ed",
-      url: "http://www.youtube.com/user/TEDEducation/videos?view=1&flow=list"
+      name: "canalplus.fr - Made in Groland",
+      url: "http://www.canalplus.fr/c-divertissement/pid1787-c-groland.html"
       }
 ];
